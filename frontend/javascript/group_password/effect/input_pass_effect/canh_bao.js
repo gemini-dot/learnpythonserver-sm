e416f09109075097dc7ddba67aa9d77838df881c
laivansam <samvasang1192011Ggmail.com>
2026-01-21 18:24:46 +0700

    styles: thêm tính năng thông báo dừng lại trong console(f12)

---
+++ frontend/javascript/group_password/effect/input_pass_effect/canh_bao.js
@@ -0,0 +1,12 @@
+function hien_canh_bao(){
+    console.log(
+  "%cDỪNG LẠI!", 
+  "color: red; font-size: 50px; font-weight: bold; text-shadow: 2px 2px 5px black;"
+);
+
+console.log(
+  "%cĐây là tính năng của trình duyệt dành cho nhà phát triển.", 
+  "font-size: 16px; color: gray;"
+);
+}
+hien_canh_bao();
