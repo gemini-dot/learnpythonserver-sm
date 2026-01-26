df200753a6d54ce84df3e4758c6976680399a884
laivansam <samvasang1192011@gmail.com>
2026-01-26 19:19:03 +0700

    test

--- backend/utils/token_het_han.py
+++ backend/utils/token_het_han.py
@@ -8,7 +8,8 @@ def kiem_tra_het_han_token(gmail,thoi_gian_tao, thoi_gian_hien_tai, khoang_thoi_
     khoang_thoi_gian_cua_nguoi_dung = thoi_gian_hien_tai - thoi_gian_tao
     if khoang_thoi_gian_cua_nguoi_dung > khoang_thoi_gian_token_hoat_dong:
         nguoi_dung = {"gmail":gmail}
-        moi = {"$set": {"trang_thai2": "da_het_han"}}
+        moi = None
+        #moi = {"$set": {"trang_thai2": "da_het_han"}}
         lay_token.update_one(nguoi_dung,moi)
         return {'success': False, 'message': 'Token đã hết hạn sử dụng'}
     return {'success': True, 'message': 'Token vẫn còn hiệu lực'}
