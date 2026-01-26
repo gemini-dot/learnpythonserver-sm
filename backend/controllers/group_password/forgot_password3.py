f18a2aed4a762a0f73758712576b0e3e5bfbc096
laivansam <samvasang1192011@gmail.com>
2026-01-26 18:54:55 +0700

    test

--- backend/controllers/group_password/forgot_password3.py
+++ backend/controllers/group_password/forgot_password3.py
@@ -3,6 +3,9 @@ from services.group_mk.forgot_password3 import kiem_tra_de_doi_mat_khau
 from logs.logger import logger
 
 def doi_mat_khau_moi():
+
+    print("đến đoạn này rồi bạn ơi, forgot_password3.py - controller")
+
     data = request.get_json()
     token = data.get('token')
     gmail = data.get('gmail')
@@ -10,6 +13,8 @@ def doi_mat_khau_moi():
 
     kiem_tra = kiem_tra_de_doi_mat_khau(token, gmail, new_password)
     
+    print("đến đoạn này rồi bạn ơi, forgot_password3.py - controller")
+
     if kiem_tra["success"]:
         return jsonify(kiem_tra), 200
     return jsonify(kiem_tra), 400
--- backend/services/group_mk/forgot_password3.py
+++ backend/services/group_mk/forgot_password3.py
@@ -8,18 +8,26 @@ def kiem_tra_de_doi_mat_khau(token, gmail, new_password):
         ket_qua = thu_muc_can_kiem_tra.find_one({'gmail': gmail})
         if not ket_qua:
             return {'success': False, 'message': 'Yêu cầu không tồn tại hoặc đã hết hạn!'}
+        
         token_trong_db = ket_qua.get('token_nguoi_dung')
         trang_thai_token = ket_qua.get('trang_thai1')
+
+        if token != token_trong_db:
+            return {'success': False, 'message': 'Token không hợp lệ!'}
         if trang_thai_token != 'sap_su_dung':
             return {'success': False, 'message': 'Token không hợp lệ để đổi mật khẩu!'}
-        ket_qua1 = thu_muc_can_kiem_tra.update_one(
+        
+        print("đến đoạn này rồi bạn ơi, forgot_password3.py")
+        
+        thu_muc_can_kiem_tra.update_one(
             {"gmail": gmail},
             {"$set": {"trang_thai1": "da_su_dung"}}
         )
-        ket_qua2 = thu_muc_nguoi_dung.update_one(
+        thu_muc_nguoi_dung.update_one(
             {"gmail": gmail},
             {"$set": {"password": new_password}}
         )
+
         return {'success': True, 'message': 'Đổi mật khẩu thành công!'}
     except Exception as e:
         logger.error(f"Lỗi khi kiểm tra để đổi mật khẩu: {e}")
