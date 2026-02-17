f39e668d5acc70af9ea1433d20b3a49d36417458
laivansam <samvasang1192011@gmail.com>
2026-02-17 20:44:03 +0700

    test: upload(4)

--- backend/utils/cloudidary_json_get.py
+++ backend/utils/cloudidary_json_get.py
@@ -2,18 +2,22 @@ def make_json_cloud(upload_result, user_email, ten_goc):
     ext = upload_result.get('format', '')
     original_name = upload_result.get('original_filename', upload_result.get('public_id', 'unnamed_file'))
     
-    if ten_goc and ten_goc != "":
-        full_name = ten_goc
+    ext_raw = upload_result.get('format') or '' 
+    ext = ext_raw.upper()
+
+    if ten_goc and ten_goc.strip() != "":
+            full_name = ten_goc
     else:
-        original_name = upload_result.get('original_filename', upload_result.get('public_id', 'unnamed_file'))
-        full_name = f"{original_name}.{ext.lower()}" if ext else original_name
+            original_name = upload_result.get('original_filename', 'unnamed_file')
+            # Chỉ thêm đuôi nếu có ext
+            full_name = f"{original_name}.{ext_raw.lower()}" if ext_raw else original_name
 
     file_info = {
         "public_id": upload_result.get('public_id'),
         "name": full_name,
         "url": upload_result.get('secure_url'),
         "size": upload_result.get('bytes', 0), 
-        "ext": ext.upper(),
+        "ext": ext,
         "type": upload_result.get('resource_type', 'raw'),
         "created_at": upload_result.get('created_at'),
         "user_gmail":user_email
