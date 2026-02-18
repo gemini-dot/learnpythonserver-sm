4c501ba41f2429b3df8b078b1a8ee352a7463a8f
laivansam <samvasang1192011@gmail.com>
2026-02-18 21:15:05 +0700

    test: delete file(3)

--- backend/utils/lay_du_lieu_thu_db.py
+++ backend/utils/lay_du_lieu_thu_db.py
@@ -4,10 +4,17 @@ def lay_tat_ca_file(user_gmail, collection):
     user_data = list(collection_can_tim.find({'user_gmail':user_gmail}))
     if not user_data:
         return list(user_data)
-    danh_sach_file = []
+    res = {
+        "danh_sach_file" : [],
+        "danh_sach_file_da_xoa":[]
+    }
+    
     for doc in user_data:
         if '_id' in doc:
             doc['_id'] = str(doc['_id'])   
             doc['id'] = doc['_id']
-        danh_sach_file.append(doc)
-    return danh_sach_file
+        if doc.get('trang_thai') == 'da_xoa':
+            res['danh_sach_file_da_xoa'].append(doc)
+        else:
+            res['danh_sach_file'].append(doc)
+    return res
