8d010992adc8c518000fe5c26eea82c596579b66
laivansam <samvasang1192011@gmail.com>
2026-02-18 20:39:29 +0700

    test: rename(8)

--- backend/controllers/group_chuc_nang/upload/chuc_nang/get_profile_name.py
+++ backend/controllers/group_chuc_nang/upload/chuc_nang/get_profile_name.py
@@ -1,11 +1,13 @@
 from flask import request, jsonify
 from services.group_chuc_nang.dashboard.get_profile_name import get_profile
 def get_profile_controller():
-    gmail = request.cookies.get("user_gmail")
-
-    ket_qua = get_profile(gmail)
-    if not gmail:
-        return jsonify({'trang_thai': False, 'mes': "Cookie gmail trống"}), 401
-    if not ket_qua['trang_thai']:
-        return jsonify(ket_qua),404
-    return ket_qua,200
+    try:
+        gmail = request.cookies.get("user_gmail")
+        if not gmail:
+            return jsonify({'trang_thai': False, 'mes': "Cookie gmail trống"}), 401
+        ket_qua = get_profile(gmail)
+        if not ket_qua['trang_thai']:
+            return jsonify(ket_qua),404
+        return ket_qua,200
+    except Exception as e:
+        return jsonify({'trang_thai': False, 'mes': f"error{e}"}),500
