e7d7e00f9bb90f7b941bdf5c87301479abb797ae
laivansam <samvasang1192011@gmail.com>
2026-02-18 23:09:27 +0700

    test: thay title(2)

---
+++ frontend/javascript/upload/dashboard/effect/thay_title.js
@@ -0,0 +1,40 @@
+function transformNameForLogo(fullName) {
+  if (!fullName || fullName === 'Anonymous') return 'MY';
+  const nameParts = fullName.trim().split(/\s+/);
+  let lastName = nameParts[nameParts.length - 1];
+
+  lastName = lastName
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/đ/g, 'd')
+    .replace(/Đ/g, 'D');
+  return lastName.toUpperCase();
+}
+
+// Hàm gọi API để lấy và hiển thị tên
+async function syncLogoWithName() {
+  try {
+    const response = await fetch(
+      'https://learnpythonserver-sm.onrender.com/profile/get_profile',
+      {
+        method: 'GET',
+        credentials: 'include',
+      }
+    );
+
+    const data = await response.json();
+
+    if (data.trang_thai && data.username) {
+      const finalName = transformNameForLogo(data.username);
+
+      const logoEl = document.querySelector('.logo');
+      if (logoEl) {
+        logoEl.innerHTML = `${finalName} <span>files</span>`;
+      }
+    }
+  } catch (error) {
+    console.error('Lỗi khi đồng bộ tên Logo:', error);
+  }
+}
+
+syncLogoWithName();
