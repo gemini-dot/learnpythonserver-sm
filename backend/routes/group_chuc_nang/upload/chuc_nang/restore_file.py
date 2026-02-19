95d3d6eeec3508abf38f779731e082cc1f2d22a3
laivansam <samvasang1192011@gmail.com>
2026-02-20 00:28:04 +0700

    fix: restore file(4)

--- backend/routes/group_chuc_nang/upload/chuc_nang/restore_file.py
+++ backend/routes/group_chuc_nang/upload/chuc_nang/restore_file.py
@@ -8,5 +8,5 @@ app_route13 = Blueprint('restore file',__name__)
 @app_route13.route('/restorefile_user',methods=['POST'])
 @limit_requests(max_requests=MAX_REQUESTS, period=PERIOD)
 
-def delete_file_route():
+def restore_file_route():
     return restore_file_services()
