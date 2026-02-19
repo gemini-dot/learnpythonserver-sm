600ca3b5f755c2ddd42a9567d50ae45de277b57d
laivansam <samvasang1192011@gmail.com>
2026-02-20 00:07:38 +0700

    update: restore file

---
+++ backend/routes/group_chuc_nang/upload/chuc_nang/restore_file.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+from middleware.rate_limiting import limit_requests
+from controllers.group_chuc_nang.upload.chuc_nang.restore_file import restore_file_services
+from configs.settings import MAX_REQUESTS, PERIOD
+
+app_route13 = Blueprint('delete file',__name__)
+
+@app_route13.route('/restorefile_user',methods=['POST'])
+@limit_requests(max_requests=MAX_REQUESTS, period=PERIOD)
+
+def delete_file_route():
+    return restore_file_services()
