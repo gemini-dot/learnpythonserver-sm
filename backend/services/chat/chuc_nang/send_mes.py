16630eb03b9a014c36710f16f706833ca323ed0d
laivansam <samvasang1192011@gmail.com>
2026-02-24 20:44:40 +0700

    fix: facebook chat(1)

--- backend/services/chat/chuc_nang/send_mes.py
+++ backend/services/chat/chuc_nang/send_mes.py
@@ -3,8 +3,14 @@ import requests
 PAGE_ACCESS_TOKEN = "EAAWQ4rWjGfoBQ6LqxaZAR643TLZBUQQCsQrkNQe0RZChhuVM9LfC6IoZB3rDKw8z75ZBm0NKM9jMnCxBWerZAolmnv7uJZAu9beVSTZBpf88nqy24NvVi4QJ54ZAgM6bEjGRcV2Ee9v7cMNUZAEC66S9idXwjddfyBOcloQOZCB0TSjnxUKT0ijH3nGHKZC6ZAjR0GQ8Gum6iPwZDZD"
 
 
+def send_typing(recipient_id):
+    url = "https://graph.facebook.com/v12.0/me/messages"
+    params = {"access_token": PAGE_ACCESS_TOKEN}
+    payload = {"recipient": {"id": recipient_id}, "sender_action": "typing_on"}
+    requests.post(url, params=params, json=payload)
+
 def send_message(recipient_id, text):
     url = "https://graph.facebook.com/v12.0/me/messages"
     params = {"access_token": PAGE_ACCESS_TOKEN}
-    payload = {"recipient": {"id": recipient_id}, "message": {"text": text},"sender_action": "typing_on"}
+    payload = {"recipient": {"id": recipient_id}, "message": {"text": text}}
     requests.post(url, params=params, json=payload)
