2ffbbc6df0cd5291ed3723e0a2bf8e8fa84b3cf7
laivansam <samvasang1192011@gmail.com>
2026-03-01 13:29:01 +0700

    update: bao mat(dung luong)

---
+++ backend/middleware/limit_content_length.py
@@ -0,0 +1,13 @@
+from flask import request, abort
+from functools import wraps
+
+def limit_content_length(max_length):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            cl = request.content_length
+            if cl is not None and cl > max_length:
+                abort(413, description=f"File quá lớn! Giới hạn là {max_length/1024/1024}MB")
+            return f(*args, **kwargs)
+        return wrapper
+    return decorator
--- backend/routes/group_password/input_pass.py
+++ backend/routes/group_password/input_pass.py
@@ -4,12 +4,12 @@ from flask import Blueprint
 from middleware.rate_limiting import limit_requests
 from controllers.group_password.input_pass import kiem_tra1
 from configs.settings import MAX_REQUESTS, PERIOD
+from middleware.limit_content_length import limit_content_length
 
 app_route = Blueprint("auth_input", __name__)
 
-app_route.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
-
 @app_route.route("/input-pass", methods=["POST"])
 @limit_requests(max_requests=MAX_REQUESTS, period=PERIOD)
+@limit_content_length(100 * 1024 * 1024)
 def input_pass():
     return kiem_tra1()
