b426b50478c5876afeac3aefd38a918204e5570c
laivansam <samvasang1192011@gmail.com>
2026-03-29 12:04:33 +0700

    update(robots.txt): content

--- backend/routes/chuyen_huong/robot/robot.py
+++ backend/routes/chuyen_huong/robot/robot.py
@@ -4,5 +4,11 @@ robot_site = Blueprint("robot_txt", __name__)
 
 @robot_site.route('/robots.txt')
 def robots_txt():
-    content = "User-agent: *\nDisallow: /admin/\nAllow: /"
+    content = """User-agent: *
+        Content-Signal: search=yes,ai-train=no
+        Allow: /
+
+        User-agent: GPTBot
+        Disallow: /
+"""
     return content, 200, {'Content-Type': 'text/plain'}
