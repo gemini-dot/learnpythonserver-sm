810a27efe37f58de568b41d584ef8d342820c0b4
admin_sam <samvasang1192011@gmail.com>
2026-04-11 23:06:17 +0700

    test(update): chuyen trang(fix - 1)

--- backend/routes/group_password/chuyen_huong/reset_password/repass.py
+++ backend/routes/group_password/chuyen_huong/reset_password/repass.py
@@ -3,6 +3,7 @@ from configs.duong_dan_thu_muc import thu_muc_chinh
 from configs.settings import MAX_REQUESTS, PERIOD
 from middleware.rate_limiting import limit_requests
 from configs.db import db
+from logs import logger
 send_mail_reset_password_main = Blueprint(
     "send_mail_reset_password route user main", __name__
 )
@@ -18,12 +19,33 @@ def send_mail_reset_password_main_user_route():
     token = session.get("token")
 
     if not token:
-        return send_from_directory(thu_muc_chinh("frontend/view/error"), "401.html"), 401
-    
-    res = token_collection.find_one({"gmail": user_gmail}, {"gmail": 1, "token_nguoi_dung": 1, "trang_thai1": 1, "trang_thai2": 1})
+        return (
+            send_from_directory(thu_muc_chinh("frontend/view/error"), "401.html"),
+            401,
+        )
 
-    if str(res.get("trang_thai1")) != "sap_su_dung" or str(res.get("trang_thai2")) != "da_het_han" or res.get("token_nguoi_dung") != token:
-        return send_from_directory(thu_muc_chinh("frontend/view/error"), "401.html"), 401
+    res = token_collection.find_one(
+        {"gmail": user_gmail},
+        {"gmail": 1, "token_nguoi_dung": 1, "trang_thai1": 1, "trang_thai2": 1},
+    )
+
+    if not res:
+        logger.debug("lỗi đây nè", __file__)
+        return (
+            send_from_directory(thu_muc_chinh("frontend/view/error"), "401.html"),
+            401,
+        )
+
+    if (
+        str(res.get("trang_thai1")) != "sap_su_dung"
+        or str(res.get("trang_thai2")) != "da_het_han"
+        or res.get("token_nguoi_dung") != token
+    ):
+        logger.debug("đến đoạn này rồi nè", __file__)
+        return (
+            send_from_directory(thu_muc_chinh("frontend/view/error"), "401.html"),
+            401,
+        )
 
     return send_from_directory(
         thu_muc_chinh("frontend/view/group_password"), "create_new_pass.html"
