8147901a0490498989a43d4dea0f48b1691efd3d
admin_sam <samvasang1192011@gmail.com>
2026-04-15 00:00:12 +0700

    test: web (fix save mistake)

---
+++ backend/services/group_chuc_nang/dashboard/get_du_lieu.py
@@ -0,0 +1,16 @@
+from configs.db import db
+
+def get_du_lieu_dashboard(user_gmail: str) -> dict:
+    collection = db['users']
+
+    user_data = collection.find_one({'gmail': user_gmail}, {'_id': 0, 'cap_nguoi_dung': 1, 'username': 1, 'luu_tru': 1, 'bio': 1})
+
+    if not user_data:
+        return {
+            'cap_nguoi_dung': 'basic',
+            'username': 'User',
+            'luu_tru': 'Basic',
+            'bio': ''
+        }
+    
+    return user_data
--- backend/templates/thay_doi.md
+++ backend/templates/thay_doi.md
@@ -3,3 +3,4 @@ user_power= quyền hạn. role người dùng
 user_storage = trạng thái dung lượng ứng với quyền hạn hiện tại của người dùng
 logo_name
 username
+user_email
