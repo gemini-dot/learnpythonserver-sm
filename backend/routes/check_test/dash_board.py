096fb54202d6c7385951917332ea42e2accf586b
admin_sam <samvasang1192011@gmail.com>
2026-04-14 21:07:52 +0700

    update: render tem(fix link cdn)

--- backend/routes/check_test/dash_board.py
+++ backend/routes/check_test/dash_board.py
@@ -5,4 +5,4 @@ dash_board = Blueprint('dash_board', __name__)
 @dash_board.route('/dash_board_test')
 
 def dash_board_test():
-    return render_template('test.html', name='lvs', user_email=session.get("user_gmail", "User"))
+    return render_template('test.html', name='lvs', user_email=session.get("user_gmail", "User"), quyen_han='basic', goi_luu_tru='Basic')
